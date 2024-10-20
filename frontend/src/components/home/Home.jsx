473eb55d8aaa33d5e763adcb3599f37535143174
Lucas Cavalcante <lucascavalcante2028@gmail.com>
2024-10-20 06:57:23 -0300

    Cadastro em React

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Main from "../template/Main";
+
+export default props=>
+    <Main icon= "home" title= "Início"
+        subtitle="Segundo projeto de React.">
+        <div className='display-4'>Bem Vindo!</div>
+        <hr />
+        <p className="mb-o">Este é um sistema que tem como função exemplificar a construção de um sistema de cadastro desenvolvido em React!</p>
+    </Main>
---
+++ frontend/src/components/template/Footer.jsx
@@ -0,0 +1,10 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+    <footer className="footer">
+        <span>
+            Desenvolvido por
+            <strong> Luca<span className="text-danger">s</span></strong>
+        </span>
+    </footer>
